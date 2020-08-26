7de0d6884cb26e7bb8df5422d61ffb68e817f51f
wladm <wladmyr@visiondevelopment.co>
2020-08-26 05:21:46 -0300

    Configuration TypeORM

---
+++ src/database/migrations/1598427939855-CreateAppointments.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export default class CreateAppointments1598427939855 implements MigrationInterface {
+  //!!SÓ PODE ALTERAR UMA MIGRATION SE ELA NÃO FOI AINDA ENVIADA PARA UM CONTROLE DE VERSÃO;
+  // Se não, tem que criar uma nova com as alterações;
+  //O que quer que seja feito no BD, quando a mesma for executada;
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          }
+        ]
+      })
+    );
+  }
+
+  //Fallback - Desfazer o método up;
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,19 +1,15 @@
-import { uuid } from "uuidv4";
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  //Receber todos os dados menos o id, que é passado por parâmetro de tipagem dentro da função Omit
-  // 1.Tipo 2.Variável;
-  constructor({ provider, date }: Omit<Appointment, "id">) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
